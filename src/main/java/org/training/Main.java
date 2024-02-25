815d6f65b56505fe5cc5d30f3b4357a25e108782
SergeevViktor <equalizersva@yandex.ru>
2024-02-25 20:07:31 +0300

    feat: count-of-elements

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -1,7 +1,26 @@
 package org.training;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] elements = {"Россия", "Франция", "Италия", "Италия", "Россия", "Китай"};
+        Map<String, Integer> map = countByElements(elements);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String key = entry.getKey() + " - ";
+            System.out.printf("%s%-5d" , key, entry.getValue());
+        }
+    }
+
+    public static <K> Map<K, Integer> countByElements(K[] elements) {
+        Map<K, Integer> map = new HashMap<>();
+
+        for (K element : elements) {
+            map.compute(element, (k, count) -> count == null ? 1 : count + 1);
+        }
+
+        return map;
     }
 }
