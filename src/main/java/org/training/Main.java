eb5078e283aa927680ee03bb8e78707b003094bd
SergeevViktor <equalizersva@yandex.ru>
2024-02-25 20:40:37 +0300

    feat: number-generation

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -1,7 +1,28 @@
 package org.training;
 
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+
+        orders.stream()
+                .collect(groupingBy(Order::getProduct, summingDouble(Order::getCost)))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Double>comparingByValue()
+                        .reversed())
+                .limit(3)
+                .forEach(System.out::println);
     }
 }
